f41ebb95c2683a8ac015f32ab0615946f09217ea
shubb <13276972+shubb@users.noreply.github.com>
2019-07-31 00:52:15 +0100

    Cleanup code a little

--- index.js
+++ index.js
@@ -5,40 +5,34 @@ const util = require('util');
 const config = require('./config.json')
 const RangeInMonths = require('./RangeInMonths')
 
-// Connect to starling using personal access token
-const client = new Starling({
-    accessToken: config.personal_token
-});
-
-//console.log(typeof(accountPromise.data.accountNumber))
-
-// Async function to 
+// Do everything in an async function so we can use await
 async function StarlingToMongo() {
-
     try {
-
-        // Mongo connection parameters
-        var MONGO_PUBLISH_IP='127.0.0.1'
-        var MONGO_PUBLISH_PORT='27017'
-        var DB_NAME='starling'
-        var MONGO_URL=`mongodb://${MONGO_PUBLISH_IP}:${MONGO_PUBLISH_PORT}`
-        var COL_TRANSACTIONS = 'transctions'
-        
         // Promisify callback function so we can use await
         const asyncMongoClientConnect = util.promisify(MongoClient.connect)
         
         // Connect to the database
-        var dbConnection = await asyncMongoClientConnect(MONGO_URL)
-        var db = dbConnection.db(DB_NAME)
-        db.dropCollection(COL_TRANSACTIONS)
-        db.createCollection(COL_TRANSACTIONS)
-        var collection = db.collection(COL_TRANSACTIONS)
+        mongo_url=`mongodb://${config.mongo.server_ip}:${config.mongo.server_port}`
+        var dbConnection = await asyncMongoClientConnect(mongo_url)
+        var db = dbConnection.db(config.mongo.database_name)
 
-        const asyncInsertMany = util.promisify(collection.insertMany.bind(collection))
-        const asyncInsertOne = util.promisify(collection.insertOne.bind(collection))
+        // Drop and recreate the destination collection
+        db.dropCollection(config.mongo.transaction_collection)
+        db.createCollection(config.mongo.transaction_collection)
+
+        // Get a reference to the destination connection
+        var transaction_collection = db.collection(config.mongo.transaction_collection)
+        // And make an async version of its insert method
+        const asyncInsertOne = util.promisify(transaction_collection.insertOne.bind(transaction_collection))
+
+        // Connect to starling using personal access token
+        const client = new Starling({
+            accessToken: config.starling.personal_token
+        });
         
+        //Retrieve basic account information
         var accountDetails = await client.getAccount()
-        
+        if(accountDetails.err) throw new Error(`Get account details failed with error: ${accountDetails.err}`);
         var accountInfo = {
             "accountNumber": accountDetails.data.accountNumber,
             "createdAt":     new Date(accountDetails.data.createdAt)
@@ -57,8 +51,8 @@ async function StarlingToMongo() {
                     period.start.format('YYYY-MM-DD'),
                     period.end.format('YYYY-MM-DD'),
                 );
-            if(response.err) throw new Error(response.err);
-            // Retrieve transactions from the response
+                if(response.err) throw new Error(`Getting transaction summaries for period  failed with error: ${response.err}`);
+            // From the response, select the transactions list
             var transactions = response.data._embedded.transactions;
 
             // For each transaction
@@ -69,66 +63,14 @@ async function StarlingToMongo() {
                         transactionSummary.id,
                         transactionSummary.source
                     )
-                if(transactionDetails.err) throw new Error(transactionDetails.err);
+                    if(transactionDetails.err) throw new Error(`Getting transaction detail for transaction ${transactionSummary.id} failed with error: ${transactionDetails.err}`);
 
                 //Write transactions to the database
                 var mongoWriteReport = await asyncInsertOne(transactionDetails.data);
-                if(mongoWriteReport.err) throw new Error(mongoWriteReport.err);                
+                if(transactionDetails.err) throw new Error(`Writing transaction to mongo failed with error: ${transactionDetails.err}`);        
             }));            
         }
     }
-    catch (err) { throw new Error(err)}
-
-    //console.log(JSON.stringify(accountInformation, null, 2))
+    catch (err) { throw new Error(`Unhandled exception in StarlingToMongo: ${err}`)}
 }
 StarlingToMongo()
-
-/*
-client.getAccount()
-/*
-Available data (synthetic sample)
-  "id": "f13d1136-0c92-3348-3ba2-8f6aa4fb9ffa",
-  "name": "a23ca446-2a2a-34ca-e31b-cc1f4a283c0 GBP",
-  "number": "71231234",
-  "accountNumber": "71231234",
-  "sortCode": "123456",
-  "currency": "GBP",
-  "iban": "GB12SRLG10123456123456",
-  "bic": "SRAAGB4T",
-  "createdAt": "2015-06-01T12:00:00.000Z"
-
-*//*
-.then(({data}) => {
-    //console.log(JSON.stringify(data, null, 2))
-    return accountInformation = {
-        "accountNumber": data.accountNumber,
-        "createdAt":     data.createdAt
-    }
-})
-.then(({accountInformation}) => {
-    client.getTransactions(undefined, '2019-06-01', '2019-07-01')
-    .then(({data}) => {
-        console.log(JSON.stringify(data, null, 2))
-    })
-    .catch(err => console.log(err));
-})
-.then(({accountInformation}) => {
-
-)
-.catch(err => console.log(err));
-
-
-
-/*
-client.getTransactions(undefined, '2019-06-01', '2019-07-01')
-    .then(({data}) => {
-        console.log(JSON.stringify(data, null, 2))
-    })
-    .catch(err => console.log(err));
-
-client.getTransaction(undefined, '2a94c52a-f463-4423-a8d7-9dbbe826a8b2', 'MASTER_CARD')
-    .then(({data}) => {
-        console.log(JSON.stringify(data, null, 2))
-    })
-    .catch(err => console.log(err));
-*/

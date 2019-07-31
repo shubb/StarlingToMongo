8e7c243b4e11dfdfde91c06871386f1fff3ff403
shubb <13276972+shubb@users.noreply.github.com>
2019-07-31 21:58:59 +0100

    Code cleanup

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const Starling = require('starling-developer-sdk');
 const MongoClient = require('mongodb').MongoClient;
 const util = require('util');
 
-const config = require('./config.json')
-const RangeInMonths = require('./RangeInMonths')
+const config = require('./config.json');
+const RangeInMonths = require('./RangeInMonths');
 
 // Do everything in an async function so we can use await
 async function StarlingToMongo() {
     try {
         // Promisify callback function so we can use await
-        const asyncMongoClientConnect = util.promisify(MongoClient.connect)
+        const asyncMongoClientConnect = util.promisify(MongoClient.connect);
         
         // Connect to the database
         mongo_url=`mongodb://${config.mongo.server_ip}:${config.mongo.server_port}`
@@ -21,9 +21,9 @@ async function StarlingToMongo() {
         db.createCollection(config.mongo.transaction_collection)
 
         // Get a reference to the destination connection
-        var transaction_collection = db.collection(config.mongo.transaction_collection)
+        var transaction_collection = db.collection(config.mongo.transaction_collection);
         // And make an async version of its insert method
-        const asyncInsertOne = util.promisify(transaction_collection.insertOne.bind(transaction_collection))
+        const asyncInsertOne = util.promisify(transaction_collection.insertOne.bind(transaction_collection));
 
         // Connect to starling using personal access token
         const client = new Starling({
@@ -31,12 +31,12 @@ async function StarlingToMongo() {
         });
         
         //Retrieve basic account information
-        var accountDetails = await client.getAccount()
+        var accountDetails = await client.getAccount();
         if(accountDetails.err) throw new Error(`Get account details failed with error: ${accountDetails.err}`);
         var accountInfo = {
             "accountNumber": accountDetails.data.accountNumber,
             "createdAt":     new Date(accountDetails.data.createdAt)
-        }
+        };
 
         // Sequentially download transaction histories
         // Note this is synchronous to be kind to their api
@@ -46,24 +46,31 @@ async function StarlingToMongo() {
         for(const period of new RangeInMonths(accountInfo.createdAt, today)) {
             
             //Get transactions for this period
-            var response = await client.getTransactions(
-                    undefined, 
-                    period.start.format('YYYY-MM-DD'),
-                    period.end.format('YYYY-MM-DD'),
-                );
-                if(response.err) throw new Error(`Getting transaction summaries for period  failed with error: ${response.err}`);
+            try{
+                var start = period.start.format('YYYY-MM-DD');
+                var end = period.end.format('YYYY-MM-DD');
+                var response = await client.getTransactions(
+                        undefined, 
+                        start,
+                        end
+                    );
+                if(response.err) throw new Error(`Getting transaction summaries for period ${start} - ${end} failed with error: ${response.err}`);
+            } catch (err) { throw new Error(`Getting transaction summaries for period ${start} - ${end} failed with error: ${err}`); }
             // From the response, select the transactions list
             var transactions = response.data._embedded.transactions;
 
             // For each transaction
             await Promise.all(transactions.map( async (transactionSummary) => {
                 // Get all details for that transaction
-                var transactionDetails = await client.getTransaction(
-                        undefined,
-                        transactionSummary.id,
-                        transactionSummary.source
-                    )
+                try{
+                    // TODO: This is failing on a different transaction each time, try rate limiting
+                    var transactionDetails = await client.getTransaction(
+                            undefined,
+                            transactionSummary.id,
+                            transactionSummary.source
+                        )
                     if(transactionDetails.err) throw new Error(`Getting transaction detail for transaction ${transactionSummary.id} failed with error: ${transactionDetails.err}`);
+                } catch (err) { throw new Error(`Getting transaction detail for transaction ${transactionSummary.id} failed with error: ${err}`); }
 
                 //Write transactions to the database
                 var mongoWriteReport = await asyncInsertOne(transactionDetails.data);
@@ -71,6 +78,6 @@ async function StarlingToMongo() {
             }));            
         }
     }
-    catch (err) { throw new Error(`Unhandled exception in StarlingToMongo: ${err}`)}
+    catch (err) { throw new Error(`Unhandled exception in StarlingToMongo: ${err}`); }
 }
 StarlingToMongo()
